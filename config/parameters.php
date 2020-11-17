67e4c91bf95b4df74a4d275c6bd985f37bb113cd
jucetina <juliethcetina@gmail.com>
2020-11-16 23:29:25 -0500

    Se actualizan las constantes para que funcione en producción

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,5 @@
 <?php
-define("base_url", "http://localhost/tienda/");
+//define("base_url", "http://localhost/tienda/");
+define("base_url", "https://mitienda-php.herokuapp.com/");
 define("default_controller", "ProductoController");
 define("default_action", "index");
