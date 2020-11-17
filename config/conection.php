0933074697c45e25ed6b9c2ae01a82c63c03ebb4
jucetina <juliethcetina@gmail.com>
2020-11-16 23:44:31 -0500

    Se modifican la conexion y constantes para producción

--- config/conection.php
+++ config/conection.php
@@ -2,7 +2,8 @@
 
 class Conexion{
     public static function conectar(){
-        $db = new mysqli('localhost', 'root', 'root', 'tienda');
+        //$db = new mysqli('localhost', 'root', 'root', 'tienda');
+        $db = new mysqli('us-cdbr-east-02.cleardb.com', 'b4c82f831e5cf3', '00dc21aa', 'heroku_0235cc1deec8300');
         $db->query("SET NAMES 'utf8'");
         return $db;
     }
